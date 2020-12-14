55319f5450643ec057e4b996561859bbad416efe
Azahar Hossain <azahar.hossain@ampada.local>
2020-12-14 15:58:44 +0100

    reverse pipe added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SortPipe } from './sort.pipe';
+import { ReversePipe } from './reverse.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SortPipe
+    SortPipe,
+    ReversePipe
   ],
   imports: [
     BrowserModule,
